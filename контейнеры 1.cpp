196ebd84bfdc79cc2b73ff3dc64ad2223cafe379
Galcov <119892223+Galcov@users.noreply.github.com>
2022-12-18 22:53:10 +0300

    Add files via upload

---
+++ контейнеры 1.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <sstream>
+#include <vector>
+#include <numeric>
+#include <string>
+using namespace std;
+
+int main()
+{
+    string str;
+    getline(cin, str);
+    istringstream ist(str);
+
+    vector<signed> vec;
+    for (signed temp; ist >> temp;)
+        vec.push_back(temp);
+
+    cout
+        << boolalpha
+        << (to_string(accumulate(vec.cbegin(), vec.cend(), 0)).size() == 2)
+        << endl;
+
+    system("pause");
+}
